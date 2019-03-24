9c3680df5c36170e4261e5f2cc8a9c96e1832e13
Aleandro Valencia <axv10@hotmail.com>
2019-03-24 13:39:42 +1300

    Updated music. Added mute buttons for sound and music.

---
+++ Assets/ToggleSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSound : MonoBehaviour
+{
+    [SerializeField] Sprite on;
+    [SerializeField] Sprite off;
+    [SerializeField] AudioSource music;
+    [SerializeField] bool findMusic;
+    Button button;
+
+    private void Start()
+    {
+        if(findMusic)
+        {
+            music = GameObject.Find("Ladybugs").GetComponent<AudioSource>();
+        }
+        button = GetComponent<Button>();
+    }
+
+
+    public void ToggleMusic()
+    {
+        if (music.volume > 0.0f)
+        {
+            music.volume = 0.0f;
+            button.image.sprite = off;
+        }
+        else
+        {
+            music.volume = 1.0f;
+            button.image.sprite = on;
+        }
+    }
+}
